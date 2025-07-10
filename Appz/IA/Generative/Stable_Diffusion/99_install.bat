44b7b3575f8e24a36d319af476719659696377a4
Marc <teste74@hotmail.fr>
2025-07-10 23:07:43 +0200

    Create 99_install.bat

---
+++ Appz/IA/Generative/Stable_Diffusion/99_install.bat
@@ -0,0 +1,53 @@
+@echo off
+
+:: ####################################################################################################################################################################
+:: # Purge #
+:: #########
+del %userprofile%\Downloads\7z2500-x64.msi 
+del %userprofile%\Downloads\Git-2.46.2-64-bit.exe
+del %userprofile%\Downloads\python-3.10.6-amd64.exe
+del %userprofile%\Downloads\vc_redist.x64.exe
+del %userprofile%\Downloads\stable-diffusion-webui-master.zip
+
+:: ####################################################################################################################################################################
+:: # Nettoyage console #
+:: #####################
+cls
+
+:: ####################################################################################################################################################################
+:: # Telechargement des fichiers #
+:: ###############################
+
+:: 7Zip
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://www.7-zip.org/a/7z2500-x64.msi
+
+:: GIT
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://github.com/git-for-windows/git/releases/download/v2.46.2.windows.1/Git-2.46.2-64-bit.exe
+
+:: Python
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://www.python.org/ftp/python/3.10.6/python-3.10.6-amd64.exe
+
+:: Visual C++
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://aka.ms/vs/16/release/vc_redist.x64.exe
+
+:: Stable Diffusion
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" https://codeload.github.com/AUTOMATIC1111/stable-diffusion-webui/zip/refs/heads/master
+
+:: Pause 
+pause
+
+:: ####################################################################################################################################################################
+:: # Installation #
+:: ################
+start /wait msiexec /i %userprofile%\Downloads\7z2500-x64.msi          /qn
+start /wait            %userprofile%\Downloads\Git-2.46.2-64-bit.exe   /SILENT   /SP-
+start /wait            %userprofile%\Downloads\python-3.10.6-amd64.exe /quiet   /passive
+start /wait            %userprofile%\Downloads\vc_redist.x64.exe       /install /quiet
+
+:: ####################################################################################################################################################################
+:: # Stable Diffusion #
+:: ####################
+rmdir /q /s D:\stable-diffusion-webui-master
+
+
+"C:\Program Files\7-Zip\7z.exe" x C:\Users\Marc\Downloads\stable-diffusion-webui-master.zip  -oD:\ -y
